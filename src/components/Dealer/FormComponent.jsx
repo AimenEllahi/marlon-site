ecf4595cf695fe9728047ebb1a2cb2f3df18a011
hananmunir <hananmunir471@gmail.com>
2024-01-14 22:41:33 +0500

    Added conditional rendering

--- src/components/Dealer/FormComponent.jsx
+++ src/components/Dealer/FormComponent.jsx
@@ -1,40 +1,33 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 
-export default function FormComponent({ fields, handleButton, buttonText }) {
+export default function FormComponent({ fields, handleChange }) {
   return (
-    <div className="bg-gray-100 w-[97%] h-full mx-auto py-6 border-t-2 rounded-md">
-      <form
-        className="grid grid-cols-1 md:grid-cols-2 gap-4"
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleButton();
-          console.log("Form submitted!");
-        }}
-      >
+    <div className='bg-gray-100 w-[97%] h-full mx-auto py-6 border-t-2 rounded-md'>
+      <form className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         {fields.map((field, index) => (
-          <div key={index} className="mb-4">
+          <div key={index} className='mb-4'>
             {field.type !== "dropdown" ? (
               <>
-                <label className="block text-md font-medium text-gray-700">
+                <label className='block text-md font-medium text-gray-700'>
                   {field.label}{" "}
-                  {field.required && <span className="text-gray-700">*</span>}
+                  {field.required && <span className='text-gray-700'>*</span>}
                 </label>
                 {field.type === "textarea" ? (
                   <textarea
-                    className="form-input mt-1 block w-full border rounded-md p-2"
-                    rows="4"
+                    className='form-input mt-1 block w-full border rounded-md p-2'
+                    rows='4'
                     placeholder={field.placeholder}
-                    onChange={field.onChange}
+                    onChange={(e) => handleChange(e, field.key)}
                     value={field.value}
                     required={field.required}
                   ></textarea>
                 ) : (
                   <input
                     type={field.type || "text"}
-                    className="form-input mt-1 block w-full border rounded-md p-2"
+                    className='form-input mt-1 block w-full border rounded-md p-2'
                     placeholder={field.placeholder}
-                    onChange={field.onChange}
+                    onChange={(e) => handleChange(e, field.key)}
                     value={field.value}
                     required={field.required}
                   />
@@ -44,7 +37,7 @@ export default function FormComponent({ fields, handleButton, buttonText }) {
               <Dropdown
                 label={field.label}
                 options={field.options}
-                onChange={field.onChange}
+                onChange={(e) => handleChange(e, field.key)}
                 value={field.value}
                 required={field.required}
               />
@@ -52,15 +45,6 @@ export default function FormComponent({ fields, handleButton, buttonText }) {
           </div>
         ))}
       </form>
-
-      <div className="col-span-2 mt-4">
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md"
-        >
-          {buttonText}
-        </button>
-      </div>
     </div>
   );
 }
